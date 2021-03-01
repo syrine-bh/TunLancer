50903a9330d2fb2c36ea406aab77460d1b2f7d09
Rihabhaddad <Rihabhaddad26@gmail.com>
2021-03-02 00:17:17 +0100

    table user+ competence

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Form\usertype;
+use App\Entity\Users;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/add_user", name="user")
+     */
+    public function add_userAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form = add('ajouter' , Submittype::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('user');
+        }}}
+return $this->render('user//add_user.html.twig', [
+'Form' => $Form->createView(),
+]);
+
+
