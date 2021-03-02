7ac24125f9d0619838be13844623f9d82f4dd0f5
hiba <hiba.farhat@esprit.tn>
2021-03-02 03:40:39 +0100

    views

--- src/Controller/ConcoursController.php
+++ src/Controller/ConcoursController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Concours;
+use App\Form\ConcoursType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,4 +19,21 @@ class ConcoursController extends AbstractController
             'controller_name' => 'ConcoursController',
         ]);
     }
+
+    /**
+     * @return Response
+     * @Route ("/concours/list",name="list")
+     */
+    public function list (){
+        $repo=$this->getDoctrine()->getRepository(Concours::class);
+        $concours=$repo->findAll();
+        return $this->render('concours/list.html.twig',['concours'=>$concours]);
+    }
+
+
+
+
+
+
+
 }
---
+++ src/Form/ConcoursType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Concours;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConcoursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sujet')
+            ->add('nom')
+            ->add('description')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Concours::class,
+        ]);
+    }
+}
