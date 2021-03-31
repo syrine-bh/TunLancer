44d6c9e015179e781cca17a5511d32d081adf9a0
hiba-farhat <hiba.farhat@esprit.tn>
2021-03-31 16:44:54 +0100

    fixture

--- migrations/Version20210331143757.php
+++ migrations/Version20210331143757.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210331125641 extends AbstractMigration
+final class Version20210331143757 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210331125641 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD test INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE quiz ADD test VARCHAR(7) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP test');
+        $this->addSql('ALTER TABLE quiz DROP test');
     }
 }
